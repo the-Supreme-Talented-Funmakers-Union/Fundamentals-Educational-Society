5cabcb38d83b24d9e624f78fdd8fa1a9da56fad1
chiyanli <richardlcy0907@gmail.com>
2024-07-04 09:01:03 -0400

    alpha

--- Assets/Resources/Scripts/SceneSwitcher.cs
+++ Assets/Resources/Scripts/SceneSwitcher.cs
@@ -3,8 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
-    public void SwitchScene(string sceneName)
+    public void SwitchScene1()
     {
-        SceneManager.LoadScene(sceneName);
+        SceneManager.LoadScene(1);
+    }
+    public void SwitchScene2()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void SwitchScene3()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
